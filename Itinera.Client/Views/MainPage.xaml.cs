a277c99360021cb5204ce4f0ac7e27c4f03f4165
Thomas Milton <perso.milton@gmail.com>
2025-03-30 17:45:04 +0200

    First commit - WIP

---
+++ Itinera.Client/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Itinera.Client
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoSignIn(object sender, EventArgs e)
+        {
+        }
+
+        private void GoCreateAccount(object sender, EventArgs e)
+        {
+        }
+    }
+
+}
