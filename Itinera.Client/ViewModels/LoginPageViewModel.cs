76b9ccab3c1f65ca921fcbb20db931968fe9c04c
Thomas Milton <perso.milton@gmail.com>
2025-03-30 19:00:25 +0200

    Add viewModel - WIP

---
+++ Itinera.Client/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Itinera.Client.ViewModels
+{
+    public class LoginPageViewModel : INotifyPropertyChanged
+    {
+        #region Variables declaration
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string _username;
+        private string _password;
+        private bool _isLoginAreaVisible;
+        #endregion
+
+        #region Commands Declaration
+        public ICommand ShowLoginAreaCommand { get; }
+        public ICommand HideLoginAreaCommand { get; }
+        public ICommand LoginCommand { get; }
+
+        #endregion
+
+        /// <summary>
+        /// Username field
+        /// </summary>
+        public string Username 
+        {
+            get => _username; 
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        /// <summary>
+        /// Password field
+        /// </summary>
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public bool IsLoginAreaVisible
+        {
+            get => _isLoginAreaVisible;
+            set
+            {
+                _isLoginAreaVisible = value;
+                OnPropertyChanged(nameof(IsLoginAreaVisible));
+            }
+        }
+
+        /// <summary>
+        /// Constructor By Default
+        /// </summary>
+        public LoginPageViewModel()
+        {
+            ShowLoginAreaCommand = new Command(() => IsLoginAreaVisible = true);
+            HideLoginAreaCommand = new Command(() => IsLoginAreaVisible = false);
+            LoginCommand = new Command(Login);
+            IsLoginAreaVisible = false;
+
+
+        }
+
+        /// <summary>
+        /// Connection method 
+        /// </summary>
+        private void Login()
+        {
+           // Logic to transfert login / password to the back, service, dto...
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
