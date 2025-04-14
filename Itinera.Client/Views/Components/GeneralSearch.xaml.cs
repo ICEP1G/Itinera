b5d28e14aad5938264f66896fad4b7d82fa8b074
Thomas Kieffer <thomaskieffer.dev@gmail.com>
2025-04-14 14:33:12 +0200

    IT-157 - Adding a second version of the component

--- Itinera.Client/Views/Components/GeneralSearch.xaml.cs
+++ Itinera.Client/Views/Components/GeneralSearch.xaml.cs
@@ -1,3 +1,7 @@
+using CommunityToolkit.Maui.Behaviors;
+using Itinera.Client.Helpers;
+using Microsoft.Maui.ApplicationModel.DataTransfer;
+
 namespace Itinera.Client.Views.Components;
 
 public partial class GeneralSearch : ContentView
@@ -6,5 +10,29 @@ public partial class GeneralSearch : ContentView
 	{
 		InitializeComponent();
 	}
+
+
+	private bool withNoBackground;
+	public bool WithNoBackground
+	{
+		get { return withNoBackground; }
+        set
+        { 
+			withNoBackground = value;
+            if (withNoBackground is true)
+            {
+                UpdateComponentStyle();
+            }
+        }
+	}
+
+
+	private void UpdateComponentStyle()
+	{
+		this.MainContainer.HeightRequest = 80;
+		this.MainContainer.BackgroundColor = Color.FromRgba(0, 0, 0, 0);
+		this.filtersIcon.Behaviors.Add(new IconTintColorBehavior() { TintColor = ResourceManager.GetColor("Secondary") });
+		this.SearchGridCtn.Shadow = new Shadow() { Radius = 8, Opacity = 0.13f, Offset = new Point(1, 2) };
+    }
 
 }
