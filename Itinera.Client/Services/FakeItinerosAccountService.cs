8767a59be4dfb77efa07c0a91310dbd07d2920b8
Thomas Milton <perso.milton@gmail.com>
2025-04-23 22:16:59 +0200

    Getting fake data for home page

---
+++ Itinera.Client/Services/FakeItinerosAccountService.cs
@@ -0,0 +1,19 @@
+﻿using Itinera.DTOs.Itineros;
+
+namespace Itinera.Client.Services
+{
+    public class FakeItinerosAccountService : IItinerosAccountService
+    {
+        private readonly FakeDataService _fakeDataService;
+
+        public FakeItinerosAccountService(FakeDataService fakeDataService)
+        {
+            _fakeDataService = fakeDataService;
+        }
+
+        public async Task<ItinerosDto> GetCurrentUserAsync()
+        {
+            return await Task.FromResult(_fakeDataService.GetItineros("currentUserId", "currentUserId"));
+        }
+    }
+}
